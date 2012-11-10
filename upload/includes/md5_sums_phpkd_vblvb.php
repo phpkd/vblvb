4f8de6ebaa3af20bfb5c7c1b37f4a06a9818339e
Omranic <me@omranic.com>
2012-11-10 17:21:44 +0000

    update changelog & md5 for the new vB_vURL

--- upload/includes/md5_sums_phpkd_vblvb.php
+++ upload/includes/md5_sums_phpkd_vblvb.php
@@ -16,25 +16,26 @@
 
 $md5_sums = array(
 	'/' => array(
-		'phpkd_vblvb.php'          => 'badc2af789a726033318f08504eefb8a',
+		'phpkd_vblvb.php'			=> 'badc2af789a726033318f08504eefb8a',
 	),
 	'/admincp' => array(
-		'phpkd_vblvb.php'          => 'badc2af789a726033318f08504eefb8a',
+		'phpkd_vblvb.php'			=> 'badc2af789a726033318f08504eefb8a',
 	),
 	'/includes/phpkd/vblvb' => array(
-		'adminfunctions.php'       => '4b09e88ea8815669fb6e4846805f8328',
-		'class_copyright.php'      => 'eb73364c0abab5c3074393465dfe3511',
-		'class_core.php'           => 'c9c06a76e10ad002fafae09c6ad88b18',
-		'class_cron.php'           => 'eb73364c0abab5c3074393465dfe3511',
-		'class_dm.php'             => 'c5fac4c6843b27bb9881e3411faa46fa',
-		'class_dml.php'            => 'c0bd84fc4a3c95a5cbb92c10dd8d630d',
-		'class_hooks.php'          => 'd786451b511082b9bc3623f0ae486458',
-		'class_init.php'           => '484333de4ec7f9bdec44a13d58f1a415',
-		'class_install.php'        => 'c6bae854db816df452ee83e09857602a',
+		'adminfunctions.php'		=> '4b09e88ea8815669fb6e4846805f8328',
+		'class_copyright.php'		=> 'eb73364c0abab5c3074393465dfe3511',
+		'class_core.php'			=> 'c9c06a76e10ad002fafae09c6ad88b18',
+		'class_cron.php'			=> 'eb73364c0abab5c3074393465dfe3511',
+		'class_dm.php'				=> 'c5fac4c6843b27bb9881e3411faa46fa',
+		'class_dml.php'				=> 'c0bd84fc4a3c95a5cbb92c10dd8d630d',
+		'class_hooks.php'			=> 'd786451b511082b9bc3623f0ae486458',
+		'class_init.php'			=> '484333de4ec7f9bdec44a13d58f1a415',
+		'class_install.php'			=> 'c6bae854db816df452ee83e09857602a',
+		'class_vurl.php'			=> 'c6bae854db816df452ee83e09857602a',
 	),
 	'/includes/xml' => array(
-		'bitfield_phpkd_vblvb.xml' => 'f7198b729a84d132ddca91c6e2835085',
-		'cpnav_phpkd_vblvb.xml'    => '94ed7f71c932c284cd98ed34be2239b0',
+		'bitfield_phpkd_vblvb.xml'	=> 'f7198b729a84d132ddca91c6e2835085',
+		'cpnav_phpkd_vblvb.xml'		=> '94ed7f71c932c284cd98ed34be2239b0',
 	),
 );
 $md5_sum_softwareid = 'phpkd_vblvb';
